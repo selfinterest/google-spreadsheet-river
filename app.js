e2c14d7701cfa866e92798ce76484e4d2bab8a9d
terrence <parliamentaryterrence@gmail.com>
2014-12-28 08:32:21 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,5 +4,17 @@
  * Time: 3:17 AM
  */
 
+var riverFactory = require("./lib/main.js");
 
+riverFactory
+    .email('759184919979-tfinm66j1hq49b3690039o8mfn60gfe3@developer.gserviceaccount.com')
+    .keyFile("./primary-documents-key-file.pem")
+    .spreadsheetName("PrimaryDocumentsTest")
+    .worksheetName("Sheet1")
+    .uniqueKey("csvid")
+    .https(true)         //use HTTPS
+;
 
+var river = riverFactory.createRiver();
+
+river.synchronize();
